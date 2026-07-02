d6b381143a91904285ee9d9368ffc5ba63eb04c5
WithIn Us AI <withinusfilms@gmail.com>
2026-07-02 05:00:31 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+```javascript
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App);
+```
