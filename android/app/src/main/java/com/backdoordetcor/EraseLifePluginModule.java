ec312290de1219aae3667257e2cedb8eee941dfb
WithIn Us AI <withinusfilms@gmail.com>
2026-07-02 05:18:01 -0600

    Create EraseLifePluginModule.java

---
+++ android/app/src/main/java/com/backdoordetcor/EraseLifePluginModule.java
@@ -0,0 +1,11 @@
+```java
+package com.backdoordetcor;
+
+import androidx.annotation.NonNull;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.mrousavy.camera.frameprocessor.Frame;
+import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
+import com.google.mediapipe.tasks.vision.imagesegmenter.ImageSegmenter;
+import com.google.mediapipe.tasks.vision.imagesegmenter.ImageSegmenterOptions;
+// ...
